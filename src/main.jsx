be9bcbc60cf993fea4c6cff4b7d2df08f7e55b13
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-04-14 12:29:54 +0200

    feat: Router configured
    Configured router for navigation between pages

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {createBrowserRouter, Route, RouterProvider} from "react-router-dom";
+import { routes } from "./router/routes.jsx";
+
+const router = createBrowserRouter(routes);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
