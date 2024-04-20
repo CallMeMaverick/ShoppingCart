04cb73dcb957482cfbdc11c9120ee310ae5c7d62
Sviatoslav Kushnierov <ksvyat21@gmail.com>
2024-04-20 10:57:26 +0200

    feat: Implement navbar
    Navbar to navigate across pages

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
+import { NavLink } from "react-router-dom";
+
 export default function HomePage() {
     return (
-        <header>
-            <h1>TechTreasure</h1>
-            <p>Powering up your digital lifestyle</p>
-        </header>
+        <>
+            <div className={"header"}>
+                <header>
+                    <h1>TechTreasure</h1>
+                    <p>Powering up your digital lifestyle</p>
+                </header>
+
+                <nav className={"navigation"}>
+                    <NavLink to={"/"} activeStyle={{color: "#efd510"}}>Home</NavLink>
+                    <NavLink to={"#"} activeStyle={{color: "#efd510"}}>Catalog</NavLink>
+                    <NavLink to={"#"} activeStyle={{color: "#efd510"}}>Cart</NavLink>
+                </nav>
+            </div>
+        </>
     )
 }
